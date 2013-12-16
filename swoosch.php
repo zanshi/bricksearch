c4abe644b4e9796c22e5e9429a5e99cf586923b4
zanshi <zanshi@live.se>
2013-12-16 17:26:31 +0100

    swoosch4

--- swoosch.php
+++ swoosch.php
@@ -1,12 +1,6 @@
 <?php
 
-// Init vars
-$LOCAL_ROOT         = "/srv/http/projekt";
-$LOCAL_REPO_NAME    = "bricksearch";
-$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
-$REMOTE_REPO        = "git@github.com:jonathanstark/my_new_site.git";
-
 
 // Clone fresh repo from github using desired local repo name and checkout the desired branch
-echo getcwd();
-echo shell_exec("cd /srv/http/projekt/bricksearch && git pull");
+
+echo shell_exec("git pull");
