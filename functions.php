e91c893322ec3bedf40d67e9865e7fdf17519499
zanshi <zanshi@live.se>
2013-12-18 14:27:52 +0100

    fix

--- functions.php
+++ functions.php
@@ -38,7 +38,7 @@ function mainSearch($con, $str, $startResult)
         WHERE sets.SetID = images.itemID
         AND images.itemTypeID = 'S'
         AND (sets.SetID LIKE '%$str%' OR sets.Setname LIKE '%$str%')
-        LIMIT $startResult 20"
+        LIMIT $startResult , 20"
     ;
 
     var_dump($startResult);
