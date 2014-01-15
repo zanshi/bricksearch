ca1b2be43a97b17b1957ccfa440636e7b503d77d
Furbee <oscar.nord@gmail.com>
2014-01-15 15:31:10 +0100

    update icons

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
 
             <!-- Social Icons -->
 			  <ul class="icons">
-				<li><a href="#"><img src="Facebook.svg" alt="Icon Facebook" height="42" width="42"/></a></li>
-				<li><a href="#"><img src="Email.svg" alt="Icon Email" height="42" width="42"/></a></li>
-				<li><a href="#"><img src="GitHub.svg" alt="Icon GitHub" height="42" width="42"/></a></li>
+				<li><a href="#"><img class="facebook" src="Facebook.svg" alt="Icon Facebook" height="42" width="42"/></a></li>
+				<li><a href="#"><img class="email" src="Email.svg" alt="Icon Email" height="42" width="42"/></a></li>
+				<li><a href="#"><img class="github" src="GitHub.svg" alt="Icon GitHub" height="42" width="42"/></a></li>
 			  </ul>
         </footer>
 
