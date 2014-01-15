e7294feff57ee8343e98faf12d08257bd263d1c1
Furbee <oscar.nord@gmail.com>
2014-01-15 14:18:19 +0100

    Update Social icons

--- footer.php
+++ footer.php
@@ -1,6 +1,13 @@
 		<div class="clear"></div>
         <footer>
             <p>© 2013 BrickSearch</p>
+
+            <!-- Social Icons -->
+						<ul class="icons">
+							<li><a href="#" class="social-facebook"><span></span></a></li>
+							<li><a href="#" class="social-github"><span>Github</span></a></li>
+							<li><a href="#" class="social-mail"><span>Email</span></a></li>
+						</ul>
         </footer>
 
     </div>
