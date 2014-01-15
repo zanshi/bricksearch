8f5df18ab64827020a842152ef5f4642a5f3a7b5
Furbee <oscar.nord@gmail.com>
2014-01-15 15:41:55 +0100

    size social icons

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
 
             <!-- Social Icons -->
 			  <ul class="icons">
-				<li><a href="#"><img src="Facebook-dark.svg" alt="Icon Facebook" height="42" width="42"/></a></li>
-				<li><a href="#"><img src="Email-dark.svg" alt="Icon Email" height="42" width="42"/></a></li>
-				<li><a href="#"><img src="GitHub-dark.svg" alt="Icon GitHub" height="42" width="42"/></a></li>
+				<li><a href="#"><img src="Facebook-dark.svg" alt="Icon Facebook" height="35" width="35"/></a></li>
+				<li><a href="#"><img src="Email-dark.svg" alt="Icon Email" height="35" width="35"/></a></li>
+				<li><a href="#"><img src="GitHub-dark.svg" alt="Icon GitHub" height="35" width="35"/></a></li>
 			  </ul>
         </footer>
 
