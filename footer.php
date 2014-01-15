92f595e6372b4a4ed4ab77bb1ba46656386d5989
Furbee <oscar.nord@gmail.com>
2014-01-15 15:00:38 +0100

    icons update

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
             <!-- Social Icons -->
 			  <ul class="icons">
 				<li><a href="#"><img src="facebook.png" /></a></li>
-				<li><a href="#"><img src="google+.png" /></a></li>
+				<li><a href="#"><img src="email.png" /></a></li>
 				<li><a href="#"><img src="github.png" /></a></li>
 			  </ul>
         </footer>
