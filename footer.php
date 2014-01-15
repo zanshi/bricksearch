9b4eca7df27508d9775ee028d8ff3459bb91d0ad
Furbee <oscar.nord@gmail.com>
2014-01-15 15:41:05 +0100

    color change

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
 
             <!-- Social Icons -->
 			  <ul class="icons">
-				<li><a href="#"><img src="Facebook.svg" alt="Icon Facebook" height="42" width="42"/></a></li>
-				<li><a href="#"><img src="Email.svg" alt="Icon Email" height="42" width="42"/></a></li>
-				<li><a href="#"><img src="GitHub.svg" alt="Icon GitHub" height="42" width="42"/></a></li>
+				<li><a href="#"><img src="Facebook-dark.svg" alt="Icon Facebook" height="42" width="42"/></a></li>
+				<li><a href="#"><img src="Email-dark.svg" alt="Icon Email" height="42" width="42"/></a></li>
+				<li><a href="#"><img src="GitHub-dark.svg" alt="Icon GitHub" height="42" width="42"/></a></li>
 			  </ul>
         </footer>
 
